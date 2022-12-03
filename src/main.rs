31c95b8e63b2d7902538e5fd0e27f4e9d766d9dd
BenMcLean981 <BenMcLean981@gmail.com>
2022-12-03 10:58:11 -0500

    Setup for day 3.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use pkg::{day_1, day_2};
+use pkg::{day_1, day_2, day_3};
 
 pub mod pkg;
 
 pub fn main() {
     day_1::print_solutions::print_solutions();
     day_2::print_solutions::print_solutions();
+    day_3::print_solutions::print_solutions();
 }
