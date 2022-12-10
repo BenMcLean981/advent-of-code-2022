1e9aeaf92d583241f4bb2b8cc7c4a03d9d1441b1
BenMcLean981 <BenMcLean981@gmail.com>
2022-12-10 16:03:34 -0500

    Setup day 4.
    I've fallen behind a bit, going to try to catch up
    today now that it is the weekend.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use pkg::{day_1, day_2, day_3};
+use pkg::{day_1, day_2, day_3, day_4};
 
 pub mod pkg;
 
@@ -6,4 +6,5 @@ pub fn main() {
     day_1::print_solutions::print_solutions();
     day_2::print_solutions::print_solutions();
     day_3::print_solutions::print_solutions();
+    day_4::print_solutions::print_solutions();
 }
