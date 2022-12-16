84c2a7faab40a928fe5e19557d2c7f5a88258e03
BenMcLean981 <BenMcLean981@gmail.com>
2022-12-16 13:55:53 -0500

    Completed part 1.

--- src/pkg/day_11/monkey.rs
+++ src/pkg/day_11/monkey.rs
@@ -1,1 +1,101 @@
+use std::{cell::RefCell, collections::VecDeque, rc::Rc, str::FromStr};
 
+#[derive(Clone)]
+pub(crate) struct Monkey {
+    items: VecDeque<usize>,
+    pub operation: Rc<RefCell<dyn Fn(usize) -> usize>>,
+    test: Rc<RefCell<dyn Fn(usize) -> bool>>,
+    true_target: usize,
+    false_target: usize,
+    pub num_inspections: usize,
+}
+
+impl Monkey {
+    pub fn determine_target(&mut self, item: usize) -> usize {
+        self.num_inspections += 1;
+        match (self.test.as_ref().borrow())(item) {
+            true => self.true_target,
+            false => self.false_target,
+        }
+    }
+
+    pub fn perform_operation(&self, item: usize) -> usize {
+        let operated = (self.operation.as_ref().borrow())(item);
+        return operated / 3;
+    }
+
+    pub fn has_items(&self) -> bool {
+        return self.items.len() > 0;
+    }
+
+    pub fn pop_item(&mut self) -> usize {
+        return self.items.pop_front().unwrap();
+    }
+
+    pub fn push_item(&mut self, item: usize) {
+        self.items.push_back(item);
+    }
+}
+
+impl From<&Vec<String>> for Monkey {
+    fn from(lines: &Vec<String>) -> Self {
+        let items = get_items(&lines[1]);
+        let operation = get_operation(&lines[2]);
+        let test = get_test(&lines[3]);
+        let true_target = get_target(&lines[4]);
+        let false_target = get_target(&lines[5]);
+
+        return Monkey {
+            items,
+            operation,
+            test,
+            true_target,
+            false_target,
+            num_inspections: 0,
+        };
+    }
+}
+
+fn get_items(line: &str) -> VecDeque<usize> {
+    let split: Vec<&str> = line.split(" ").collect();
+
+    let clean_nums: Vec<String> =
+        split[4..].iter().map(|s| s.replace(",", "")).collect();
+
+    return clean_nums
+        .iter()
+        .map(|s| usize::from_str(&s).unwrap())
+        .collect();
+}
+
+fn get_operation(line: &str) -> Rc<RefCell<dyn Fn(usize) -> usize>> {
+    let split: Vec<&str> = line.split(" ").collect();
+
+    let arg = split[split.len() - 1];
+    let arg = usize::from_str(arg);
+
+    let op = split[split.len() - 2];
+
+    // can either change by arg or a the old value
+    match op {
+        "*" => Rc::new(RefCell::new(move |x| x * arg.as_ref().unwrap_or(&x))),
+        "+" => Rc::new(RefCell::new(move |x| x + arg.as_ref().unwrap_or(&x))),
+        _ => panic!("Unsupported operation type={}.", op),
+    }
+}
+
+fn get_test(line: &str) -> Rc<RefCell<dyn Fn(usize) -> bool>> {
+    let split: Vec<&str> = line.split(" ").collect();
+
+    let arg = split[split.len() - 1];
+    let arg = usize::from_str(arg).unwrap();
+
+    return Rc::new(RefCell::new(move |x| x % arg == 0));
+}
+
+fn get_target(line: &str) -> usize {
+    let split: Vec<&str> = line.split(" ").collect();
+    let last = &split[split.len() - 1];
+
+    return usize::from_str(last).unwrap();
+}
--- src/pkg/day_11/print_solutions.rs
+++ src/pkg/day_11/print_solutions.rs
@@ -1,5 +1,84 @@
+use std::io::BufRead;
+
+use crate::pkg::utils::get_file_reader;
+
+use super::monkey::Monkey;
+
 pub fn print_solutions() {
-    let filename = "./src/pkg/day_11/test.txt";
+    let filename = "./src/pkg/day_11/input.txt";
+
+    let monkeys = get_monkeys(filename);
+    let simulated = simualte_monkeys(monkeys, 20);
+    let monkey_business = get_monkey_business(simulated);
 
     println!("Day 11:");
+    println!(
+        "The monkey business after 20 rounds is {}.",
+        monkey_business
+    )
+}
+
+fn get_monkeys(filename: &str) -> Vec<Monkey> {
+    let monkey_lines = group_monkeys(filename);
+
+    return monkey_lines.iter().map(|l| Monkey::from(l)).collect();
+}
+
+fn group_monkeys(filename: &str) -> Vec<Vec<String>> {
+    let reader = get_file_reader(filename);
+
+    let mut monkey_lines: Vec<Vec<String>> = vec![];
+    let mut next_monkey: Vec<String> = vec![];
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.trim() == "" {
+            monkey_lines.push(next_monkey);
+            next_monkey = vec![];
+        } else {
+            next_monkey.push(line);
+        }
+    }
+
+    if next_monkey.len() != 0 {
+        monkey_lines.push(next_monkey)
+    }
+
+    return monkey_lines;
+}
+
+fn simualte_monkeys(monkeys: Vec<Monkey>, rounds: usize) -> Vec<Monkey> {
+    let mut monkeys = monkeys;
+
+    for _ in 0..rounds {
+        monkeys = run_round(monkeys);
+    }
+
+    return monkeys;
+}
+
+fn run_round(monkeys: Vec<Monkey>) -> Vec<Monkey> {
+    let mut next_monkeys = monkeys.clone();
+
+    for i in 0..next_monkeys.len() {
+        while next_monkeys[i].has_items() {
+            let item = next_monkeys[i].pop_item();
+
+            let item = next_monkeys[i].perform_operation(item);
+            let target = next_monkeys[i].determine_target(item);
+
+            next_monkeys[target].push_item(item);
+        }
+    }
+
+    return next_monkeys;
+}
+
+fn get_monkey_business(monkeys: Vec<Monkey>) -> usize {
+    let mut scores: Vec<usize> =
+        monkeys.iter().map(|m| m.num_inspections).collect();
+
+    scores.sort();
+
+    return scores[scores.len() - 1] * scores[scores.len() - 2];
 }
